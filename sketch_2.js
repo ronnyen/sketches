d05ceffea6be4d193a47955a63eee11a3f800b7b
Ronny Engelsman <ronny.engelsman@gmail.com>
2024-03-23 20:25:31 +0200

    Add files via upload

---
+++ sketch_2.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(100, 100);
+
+  background(200);
+
+  line(0, 0, 100, 100);
+  line(0, 100, 100, 0);
+  line(25, 0, 75, 100);
+  line(75, 0, 25, 100);
+  line(0, 25, 100, 75);
+  line(0, 75, 100, 25);
+
+}
